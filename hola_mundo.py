5336fb021de8719880b154a3c15d3daf602dd470
CarolinaC-ML <paterpatronus@outlook.de>
2021-10-28 18:47:28 -0300

    Fix bad repo

---
+++ hola_mundo.py
@@ -0,0 +1,21 @@
+#comentario inicial de una línea: mi primer hola mundo
+print('hola mundo Carolina')
+
+#declaración de variables
+var0='hola'
+var1= ' Carolina'
+var2= 0
+var3 = 1,2
+var4= 1.2
+var5 = True
+var6 = [2,3,4,'3']
+
+'''
+comentario multilínea
+comenzamos con las Acciones:
+print: muestra datos en la pantalla de la consola
+type: detecta el tipo de variable
+'''
+print(var0,var3)
+print('te saludo: ' + var0)
+print(type(var5))
