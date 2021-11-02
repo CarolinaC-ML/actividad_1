a78a55dbb2203403c94265de375d1aa4eeaa625d
CarolinaC-ML <paterpatronus@outlook.de>
2021-11-02 17:05:46 -0300

    aa

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -7,9 +7,12 @@ name = input('¿Cuál es su nombre de pila?')
 surname = input('¿y su apellido?')
 age = int(input('¿Cuántos años tiene?'))
 address = input('¿Cuál es su dirección?')
-year = int(input('¿Qué año nació?'))
+print('Introduzca su fecha de nacimiento con el formato siguiente: día/mes/año')
+date_entry = input()
+day, month, year = map(int, date_entry.split('/'))
 phone = input('¿Cuál es su teléfono?')
 email = input('¿Cómo es su correo electrónico?')
 yearNow = str(age+year)
-print('Estamos en el año ' + yearNow + '. Hola, ' + name +' '+ surname + '. Veo que vive en ' + address + 
-'. Le llamaremos al número ' + phone + ' o le escribiremos un correo a la dirección ' + email + '.')
+print('Si ya ha sido mi cumple, estamos en el año ' + yearNow + 'Si no, un año más. Hola, me llamo ' + name +' '+ surname + '. vivo en '
+ + address + '. Nací el ' + date_entry + '. Llámenme al número ' + phone + ' o escríbame a la dirección ' + email + '.')
+
